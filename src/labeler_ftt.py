faeee7b72043f8865b5c376e68cdff171fad4da1
max kolomeychenko <maxim.kolomeychenko@gmail.com>
2020-11-03 17:29:58 +0200

    debug prints

--- src/labeler_ftt.py
+++ src/labeler_ftt.py
@@ -45,17 +45,19 @@ def preprocessing(api: sly.Api, task_id, context, state, app_logger):
         data_row.append(accepted_items_count(job))
         data_row.append(rejected_items_count(job))
 
+
         # print for debug
-        #print(job.name)
-        #print(job.status)
-        #print(accepted_items_count(job), "/", labeled_items_count(job))
+        print("-----------")
+        print(job.name)
+        print(job.status)
+        print(accepted_items_count(job), "/", labeled_items_count(job))
         ftt = 0
         if accepted_items_count(job) == 0 or labeled_items_count(job) == 0:
             ftt = 0
         else:
             ftt = round(accepted_items_count(job) * 100 / labeled_items_count(job), 2)
             non_zero_ftt.append(ftt)
-        #print(ftt)
+        print(ftt)
         data_row.append(ftt)
         data.append(data_row)
 
